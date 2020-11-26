9c3c53d0af2d0474ddd0aee981a8a57701e689fc
Wataru Maeda <w.maeda@neogenia.co.jp>
2020-11-26 21:44:13 +0900

    feat: WebAPI のレスポンスとして終値の探索値を使う。エラー時に要素を省略しない

--- rails/app/stock_api/app/controllers/inheritance_controller.rb
+++ rails/app/stock_api/app/controllers/inheritance_controller.rb
@@ -10,29 +10,41 @@ class InheritanceController < ApplicationController
     dt1 = dt0 - 1.month
     dt2 = dt0 - 2.month
 
+    ps = _get_closing_prices codes, dt
     q0 = StockQuotation.where(code: codes).where(m: dt0).to_a.group_by(&:code)
     q1 = StockQuotation.where(code: codes).where(m: dt1).to_a.group_by(&:code)
     q2 = StockQuotation.where(code: codes).where(m: dt2).to_a.group_by(&:code)
 
-    render xml: create_xml(codes, q0, q1, q2)
+    render xml: _create_xml(codes, ps, q0, q1, q2)
   end
 
-  def create_xml(codes, q0, q1, q2)
+  private
+
+  # 終値を取得する
+  # @param codes [Array<String>] 証券コードの配列
+  # @return [Hash<String, BigDecimal>] {証券コード => 株価} なハッシュ
+  def _get_closing_prices(codes, dt)
+    f = Stocks::StockPriceFinder.new
+    ret = {}
+    codes.uniq.each do |code|
+      ret[code] = f.get_stock_price_by_date(code, dt)
+    end
+    ret
+  end
+
+  def _create_xml(codes, ps, q0, q1, q2)
     Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
       xml.results do
         codes.each do |code|
           q = (q0[code] || q1[code] || q2[code])&.first  # 代表データを検索
           xml.record do
             xml.code code
-            if q
-              xml.name q.name
-              xml.closing_price q0[code]&.first&.close
-              xml.for_month q0[code]&.first&.avg_closing
-              xml.for_previous_month q1[code]&.first&.avg_closing
-              xml.for_previous_two_months q2[code]&.first&.avg_closing
-            else
-              xml.error "code '#{code}' not found."
-            end
+            xml.name q&.name
+            xml.closing_price ps[code]
+            xml.for_month q0[code]&.first&.avg_closing
+            xml.for_previous_month q1[code]&.first&.avg_closing
+            xml.for_previous_two_months q2[code]&.first&.avg_closing
+            xml.error "code '#{code}' not found." unless q
           end
         end
       end
