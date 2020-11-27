8e3fd0d76d30228411a90ab3cadd616e55bb138c
Wataru Maeda <w.maeda@neogenia.co.jp>
2020-11-27 22:12:30 +0900

    feat: 銘柄名での検索機能を実装

--- rails/app/stock_api/app/controllers/inheritance_controller.rb
+++ rails/app/stock_api/app/controllers/inheritance_controller.rb
@@ -4,52 +4,77 @@ class InheritanceController < ApplicationController
   skip_forgery_protection  # CSRF検証を無効化
 
   def query
-    codes = params[:codes].split(',')
+    words = params[:codes].split(',').map{|code| Util::StringNormalizer.normalize code}
     dt = params[:date].to_date
     dt0 = Date.new dt.year, dt.month, 1
     dt1 = dt0 - 1.month
     dt2 = dt0 - 2.month
 
-    q0 = StockQuotation.get_grouped(codes, dt0)
-    q1 = StockQuotation.get_grouped(codes, dt1)
-    q2 = StockQuotation.get_grouped(codes, dt2)
+    # 3ヶ月分の月間相場情報を検索
+    # TODO: 日付を複数指定して呼び出しを１回だけにできないか？
+    q0 = StockQuotation.get_grouped(words, dt0)
+    q1 = StockQuotation.get_grouped(words, dt1)
+    q2 = StockQuotation.get_grouped(words, dt2)
+
+    # 証券コードだけを集める
+    codes = [q0, q1, q2].map(&:values).flatten.select{|sp| sp.is_a? StockQuotation }.map(&:code).uniq
+
+    # 当日終値を検索
     ps = _get_closing_prices codes, dt
 
-    render xml: _create_xml(codes, ps, q0, q1, q2)
+    render xml: _create_xml(words, ps, q0, q1, q2)
   end
 
   private
 
   # 終値を取得する
   # @param codes [Array<String>] 証券コードの配列
+  # @param dt [Date] 検索対象日付
   # @return [Hash<String, BigDecimal>] {証券コード => 株価} なハッシュ
   def _get_closing_prices(codes, dt)
     f = Stocks::StockPriceFinder.new
     ret = {}
-    codes.uniq.each do |code|
+    codes.each do |code|
       ret[code] = f.get_stock_price_by_date(code, dt)
     end
     ret
   end
 
-  def _create_xml(codes, ps, q0, q1, q2)
+  def _create_xml(words, ps, q0, q1, q2)
     err_msg = I18n.t 'errors.stocks.stock_splitted'
+    err_msg2 = I18n.t 'errors.stocks.too_many_hits'
 
     Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
       xml.results do
-        codes.each do |code|
-          q = (q0[code] || q1[code] || q2[code])&.first  # 代表データを検索
-          for_month = q0.single(code, :avg_closing, err_msg) # 当月の月間平均
-          closing_price = for_month==err_msg ? err_msg : ps[code] # 月間平均がエラーならその日の終値もエラーにする
+        words.each do |word|
+          q = q0[word] || q1[word] || q2[word]  # 代表データ
+
+          # 検索失敗判定
+          msg = if q.nil?
+                  I18n.t 'errors.stocks.not_found', word: word
+                elsif q == :too_many_hits
+                  err_msg2
+                end
 
           xml.record do
-            xml.code code
-            xml.name q&.name
-            xml.closing_price closing_price
-            xml.for_month for_month
-            xml.for_previous_month q1.single(code, :avg_closing, err_msg)
-            xml.for_previous_two_months q2.single(code, :avg_closing, err_msg)
-            xml.error "code '#{code}' not found." unless q
+            if msg
+              xml.code ''
+              xml.name word
+              xml.closing_price ''
+              xml.for_month ''
+              xml.for_previous_month ''
+              xml.for_previous_two_months ''
+              xml.error msg
+            else
+              for_month = q0.single(word, :avg_closing, err_msg) # 当月の月間平均
+              closing_price = for_month==err_msg ? err_msg : ps[q.first.code] # 月間平均がエラーならその日の終値もエラーにする
+              xml.code q.first.code
+              xml.name q.first.name
+              xml.closing_price closing_price
+              xml.for_month for_month
+              xml.for_previous_month q1.single(word, :avg_closing, err_msg)
+              xml.for_previous_two_months q2.single(word, :avg_closing, err_msg)
+            end
           end
         end
       end
--- rails/app/stock_api/app/models/stock_quotation.rb
+++ rails/app/stock_api/app/models/stock_quotation.rb
@@ -5,9 +5,44 @@ class StockQuotation < ApplicationRecord
     where(code: code).where(m: month)
   end
 
+  scope :normalized_name_and_month, -> (name, month) do
+    where('normalized_name like ?', "%#{name}%").where(m: month)
+  end
+
   class << self
-    def get_grouped(code, month)
-      h = StockQuotation.code_and_month(code, month).to_a.group_by(&:code)
+    # 検索を実行し、検索条件ごとに結果をまとめてハッシュ化して返す
+    # 検索結果が複数件だった場合は :too_many_hits が検索結果にセットされる
+    # @param words [Array<String>] 検索ワードの配列
+    # @param month [Date] 検索対象月（日付部分は1日とすること）
+    # @return [Hash<String, StockPrice>]  { 検索ワード => 検索結果 } なハッシュ
+    def get_grouped(words, month)
+      h = {}        # 戻り値
+      codes = {}    # 証券コード検索用の検索キー保持用
+      [words].flatten.map{ |word| [word, Util::StringNormalizer.normalize(word)] }.uniq.each do |word, normalized|
+        if normalized.match /\d{4}/
+          # 数字４桁であれば証券コードとみなし、あとでまとめて検索
+          codes[normalized] = word
+        else
+          # 名称で部分一致検索
+          result = StockQuotation.normalized_name_and_month(normalized, month)
+          case result.count <=> 1
+          when 0  # 1件だけヒットしたとき
+            h[word] = result
+          when 1  # 複数件ヒットしたとき
+            h[word] = :too_many_hits
+          end
+        end
+      end
+
+      if codes.present?
+        # 証券コードで検索
+        StockQuotation.code_and_month(codes.keys, month).each do |sq|
+          word = codes[sq.code]
+          (h[word] ||= []) << sq
+        end
+      end
+
+      # 戻り値に single() というメソッドを生やす
       h.define_singleton_method(:single) do |key, method=nil, error_msg=nil|
         a = h[key]
         if a.nil?
