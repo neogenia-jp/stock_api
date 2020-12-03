342ec987de2628726357f4ea1a03b6c06f6413a4
Wataru Maeda <w.maeda@neogenia.co.jp>
2020-12-03 11:12:37 +0900

    fix: bundle コマンドを www-data ユーザで実行する方変更

--- rails/docker/resources/scripts/start_debug_webapp.sh
+++ rails/docker/resources/scripts/start_debug_webapp.sh
@@ -4,7 +4,7 @@ $SCRIPTS_ROOT_DIR/stop_rails_app.sh > /dev/null
 export DEBUG=manual
 export HOME=$RAILS_ROOT_DIR
 cd $HOME
-su www-data -c 'yarn install --check-files'
-bundle install > /dev/null 2>&1
-bundle exec puma --bind tcp://0.0.0.0:$RAILS_PORT
+su www-data -c 'yarn install --check-files' \
+ && su www-data -c 'bundle install' \
+ && su www-data -c "bundle exec puma --bind tcp://0.0.0.0:$RAILS_PORT"
 
