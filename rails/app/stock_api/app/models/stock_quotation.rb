695e143175a6edfd528b5d4c1b637a89d3e56ad1
Wataru Maeda <w.maeda@neogenia.co.jp>
2024-09-10 18:31:40 +0900

    feat: StockQuotation.open_day にバリデーションを追加

--- rails/app/stock_api/app/models/stock_quotation.rb
+++ rails/app/stock_api/app/models/stock_quotation.rb
@@ -1,6 +1,9 @@
 class StockQuotation < ApplicationRecord
   self.primary_key = [:code, :m, :open_day]
 
+  # open_day は 1〜31 のみ入力可とする
+  validates :open_day, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 31 }
+
   scope :code_and_month, -> (code, month) do
     where(code: code).where(m: month)
   end
