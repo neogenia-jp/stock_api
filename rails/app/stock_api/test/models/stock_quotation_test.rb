87b6912072d72c5326ff0431b582719cb5d4d92a
Wataru Maeda <w.maeda@neogenia.co.jp>
2024-09-11 18:02:02 +0900

    test: avg_closing のバリデーションのテストを追加

--- rails/app/stock_api/test/models/stock_quotation_test.rb
+++ rails/app/stock_api/test/models/stock_quotation_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class StockQuotationTest < ActiveSupport::TestCase
+  test "avg_closing が未入力ならバリデーションエラーになること" do
+    instance = StockQuotation.new
+    
+    assert_not instance.valid?
+
+    instance.avg_closing = 1
+    assert instance.valid?
+  end
   test "open_day が32以上はバリデーションエラーになること" do
     instance = StockQuotation.new
+    instance.avg_closing = 1
     
     instance.open_day = 32
     assert_not instance.valid?
