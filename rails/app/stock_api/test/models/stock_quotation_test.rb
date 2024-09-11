208aa144eade1b5604937a05bdd3bcbc0f57612e
Wataru Maeda <w.maeda@neogenia.co.jp>
2024-09-11 17:48:02 +0900

    test: stock_quotationのバリデーションテストを書いた

--- rails/app/stock_api/test/models/stock_quotation_test.rb
+++ rails/app/stock_api/test/models/stock_quotation_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class StockQuotationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "open_day が32以上はバリデーションエラーになること" do
+    instance = StockQuotation.new
+    
+    instance.open_day = 32
+    assert_not instance.valid?
+
+    instance.open_day = 31
+    assert instance.valid?
+  end
 end
